527fe977e289fe6fc9ea9396edd992775f2d2ca1
unknown <sas@.(none)>
2011-12-08 14:45:09 +0600

    add SystemResetMessage equals() and toString()

--- Firmata/src/main/java/name/antonsmirnov/firmata/message/SystemResetMessage.java
+++ Firmata/src/main/java/name/antonsmirnov/firmata/message/SystemResetMessage.java
@@ -8,4 +8,17 @@ public class SystemResetMessage extends Message {
     public SystemResetMessage() {
         super();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+
+        return obj instanceof SystemResetMessage;
+    }
+
+    @Override
+    public String toString() {
+        return "SystemResetMessage[]";
+    }
 }
